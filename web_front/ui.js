9f5d30e4b6ae2fe99b00c09e9783e80070d16281
pttkr <ptt_kr@outlook.jp>
2020-10-31 13:52:24 +0900

    ストップウォッチ

--- web_front/ui.js
+++ web_front/ui.js
@@ -19,4 +19,42 @@ var testdrum = new Vue({
             drum1.play();
         }
     }
-})
+});
+
+var controller = new Vue({
+    el: "#controller",
+    data:{
+        // requestAnimationFrame(cb)の返り値(requestID)が入る
+        animateFrame: 0,
+
+        startTime: 0,
+        nowTime: 0,
+        diffTime: 0,
+        isPlaying: false
+    },
+    methods:{
+        play: function(event){
+            console.log("play");
+            this.isPlaying = true;
+            // ミリ秒単位で時刻を取得
+            this.startTime = Math.floor(performance.now());
+            console.log(this.startTime);
+            // ループ処理
+            // スコープが変わるのでthisを保持しておく
+            ctrl = this;
+            (function loop(){
+                ctrl.nowTime = Math.floor(performance.now());
+                ctrl.diffTime = ctrl.nowTime - ctrl.startTime;
+                ctrl.animateFrame = requestAnimationFrame(loop);
+            }());
+        },
+        stop: function(event){ 
+            console.log("stop");
+            cancelAnimationFrame(this.animateFrame);
+            this.isPlaying = false;
+            this.startTime = 0;
+            this.nowTime = 0;
+            this.diffTime = 0;
+        }
+    }
+});
