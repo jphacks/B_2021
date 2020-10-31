b9dc13015aee876d0f2498968e6e7968bcaebea9
pttkr <ptt_kr@outlook.jp>
2020-10-31 14:41:01 +0900

    pauseを追加

--- web_front/ui.js
+++ web_front/ui.js
@@ -8,15 +8,26 @@ var hello = new Vue({
 });
 
 // クリックするとドラム音が鳴るボタンテスト
+// 位置座標取得もつけた
+let screenx = "";
+let screeny = "";
 var testdrum = new Vue({
     el: "#wrapper",
     data: {
+        screenx:"x座標",
+        screeny:"y座標"
     },
     methods: {
         test: function(event){
             var drum1 = new Audio("./audio/drum1.wav");
             drum1.currentTime = 0;
             drum1.play();
+        },
+        mouse:function(event){
+            console.log("HELLO");
+            this.screenx = event.clientX;
+            this.screeny = event.clientY;
+            console.log(event.clientX,event.clientY);
         }
     }
 });
@@ -27,17 +38,18 @@ var controller = new Vue({
     data:{
         // requestAnimationFrame(cb)の返り値(requestID)が入る
         animateFrame: 0,
-
+        // 時間管理
         startTime: 0,
         nowTime: 0,
         diffTime: 0,
+        // 再生位置
+        nowPosition: 0,
+        pausePosition: 0,
+
         isPlaying: false
     },
     methods:{
         play: function(event){
-            if(this.isPlaying){
-                return;
-            }
             console.log("play");
             this.isPlaying = true;
             // ミリ秒単位で時刻を取得
@@ -49,19 +61,28 @@ var controller = new Vue({
             (function loop(){
                 ctrl.nowTime = Math.floor(performance.now());
                 ctrl.diffTime = ctrl.nowTime - ctrl.startTime;
+
+                ctrl.nowPosition = ctrl.pausePosition + ctrl.diffTime;
+
                 ctrl.animateFrame = requestAnimationFrame(loop);
             }());
         },
         stop: function(event){
-            if(!this.isPlaying){
-                return;
-            }
             console.log("stop");
             cancelAnimationFrame(this.animateFrame);
             this.isPlaying = false;
             this.startTime = 0;
             this.nowTime = 0;
             this.diffTime = 0;
+            this.nowPosition = 0;
+            this.pausePosition = 0;
+        },
+        pause: function(event){
+            console.log("pause");
+            cancelAnimationFrame(this.animateFrame);
+            this.isPlaying = false;
+            this.pausePosition = this.nowPosition;
         }
     }
 });
+
